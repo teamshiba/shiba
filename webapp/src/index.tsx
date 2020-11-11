1d7c1e7525d1440e02f6513ecbf4f5fa74f2d393
Chiqu Li <chiquli@163.com>
2020-11-11 02:18:58 -0500

    feat: add user info to firestore when signing in

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,32 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Router, Switch} from 'react-router'
 // import {createBrowserHistory} from 'history';
+import UserProvider from "./providers/UserProvider";
 import './index.css';
 import Layout from "./components/Layout";
 import App from './App';
 import Authentication from "./pages/Authentication/index"
 import reportWebVitals from './reportWebVitals';
-import { browserHistory } from './common/utils';
+import {browserHistory} from './common/utils';
 
 ReactDOM.render(
     <React.StrictMode>
         <Router history={browserHistory}>
-            <Switch>
-                <Route path="/" component={App} exact={true}/>
-                <Route path="/404" component={App}/>
-                <Route exact path="/auth" component={Authentication} />
-                <Layout>
-                    <Route path={"/room/"}>
-                        <Route path={'active'}/>
-                        <Route path={'history'}/>
-                    </Route>
-                    <Route path={"/room/:id"}>
-                    </Route>
-                    <Route path={"/user/"}>
-                        <Route path={'profile'}/>
-                    </Route>
-                </Layout>
-            </Switch>
+            <UserProvider>
+                <Switch>
+                    <Route path="/" component={App} exact={true}/>
+                    <Route path="/404" component={App}/>
+                    <Route exact path="/auth" component={Authentication}/>
+                    <Layout>
+                        <Route path={"/room/"}>
+                            <Route path={'active'}/>
+                            <Route path={'history'}/>
+                        </Route>
+                        <Route path={"/room/:id"}>
+                        </Route>
+                        <Route path={"/user/"}>
+                            <Route path={'profile'}/>
+                        </Route>
+                    </Layout>
+                </Switch>
+            </UserProvider>
         </Router>
     </React.StrictMode>,
     document.getElementById('root')
