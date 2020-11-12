373478d7eec5314307fcbc9762718eadc04a0e10
Chiqu Li <chiquli@163.com>
2020-11-12 01:02:41 -0500

    fix: delete db config file

--- service/__init__.py
+++ service/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate('fbConfig.json')
+cred = credentials.Certificate('fbConfigs.json')
 firebase = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
