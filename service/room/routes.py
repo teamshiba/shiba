faa6b0b3a498fdc05930409930bf68805e5234f6
Chiqu Li <chiquli@163.com>
2020-11-13 12:25:17 -0500

    fix: fix get_group_list api

--- service/room/routes.py
+++ service/room/routes.py
@@ -7,7 +7,8 @@ user_ref = db.collection(u'Users')
 group_ref = db.collection(u'Groups')
 
 
-def group_data_to_dict(gid: str, data: dict):
+def group_data_to_dict(record):
+    gid, data = record
     return {
         "groupId": gid,
         "displayName": data.get('roomName'),
@@ -24,27 +25,27 @@ def get_group_list():
     filter_completed = request.args.get('state') if 'state' in request.args else None
     query = group_ref.where(u'members', u'array_contains', user_id)
     if filter_completed is not None:
-        query = query.where(u'is_completed', u'=', filter_completed)
+        query = query.where(u'is_completed', u'==', filter_completed)
     results = query.stream()
     data = map(lambda doc: (doc.id, doc.to_dict()), results)
     return {
-        "data": map(group_data_to_dict, data)
+        "data": list(map(group_data_to_dict, data))
     }
 
 
-@room.route('/room', methods=['POST'])
-def create_group():
-    pass
-
-
-@room.route('/room', methods=['GET'])
-def get_group_profile():
-    pass
-
-
-@room.route('/room', methods=['PUT'])
-def update_group_profile():
-    pass
+# @room.route('/room', methods=['POST'])
+# def create_group():
+#     pass
+#
+#
+# @room.route('/room', methods=['GET'])
+# def get_group_profile():
+#     pass
+#
+#
+# @room.route('/room', methods=['PUT'])
+# def update_group_profile():
+#     pass
 
 
 @room.route('/room/join', methods=['PUT'])
