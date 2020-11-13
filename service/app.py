852f64504d2f28ad4a507a73f18294260ab77c5d
Chiqu Li <chiquli@163.com>
2020-11-13 12:03:36 -0500

    bugs: modified app.py"

--- service/app.py
+++ service/app.py
@@ -1,12 +1,6 @@
-from flask import Flask
+from service import create_app
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return "Hello, World"
-
+app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
