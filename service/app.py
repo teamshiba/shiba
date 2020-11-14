8773b31501ce4f25a3839f5ee1730aee028db666
Jianing <samosatar@gmail.com>
2020-11-14 23:02:03 +0800

    fix: exception handler in service

--- service/app.py
+++ service/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from room.routes import room
-
+from werkzeug.exceptions import HTTPException
+from utils.exceptions import handle_http_exception
 
 def create_app():
     _app = Flask(__name__)
 
     _app.register_blueprint(room)
+    _app.register_error_handler(HTTPException, handle_http_exception)
 
     return _app
 
---
+++ service/utils/exceptions.py
@@ -0,0 +1,31 @@
+from werkzeug.exceptions import HTTPException
+import json
+
+
+class InvalidQueryParams(HTTPException):
+    def __init__(self, desc=""):
+        self.code = 400
+        self.description = desc
+
+
+class InvalidRequestBody(HTTPException):
+    def __init__(self, desc=""):
+        self.code = 400
+        self.description = desc
+
+
+def handle_http_exception(e: HTTPException):
+    """Return JSON instead of HTML for HTTP errors."""
+    return {
+        "code": e.code,
+        "name": e.name,
+        "msg": e.description
+    }, e.code
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "msg": e.description
+    })
+    response.content_type = "application/json"
+    return response, e.code
