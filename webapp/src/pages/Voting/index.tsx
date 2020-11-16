24ef836c2156254aacc4165bf3a289d2d5afc6a1
yifei <yifei529@gmail.com>
2020-11-15 19:02:49 -0500

    feat: wire mobx to voting

--- webapp/src/pages/Voting/index.tsx
+++ webapp/src/pages/Voting/index.tsx
@@ -2,76 +2,64 @@
  * @desc when user need to add items to the list for voting.
  */
 
-import React, {FC, Fragment, useState} from "react";
+import React, {FC, Fragment, useContext, useEffect} from "react";
 import Header from "../../components/Header";
 import IconButton from '@material-ui/core/IconButton';
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
 import TinderCard from 'react-tinder-card'
+import VotingStore from "../../stores/voting-store";
 import './index.css'
+import {observer} from "mobx-react";
+import {RouteComponentProps} from "react-router";
 
-const db = [
-  {
-    name: 'Ginza Kyubey',
-    imgURL: 'https://jw-webmagazine.com/wp-content/uploads/2019/06/jw-5d146db6e10238.04380328.jpeg',
-    location: 'Ginza, Tokyo'
-  },
-  {
-    name: 'The MOON',
-    imgURL: 'https://lh3.googleusercontent.com/proxy/aPjtJTaEKNXQMQHv6pSgfq94CZXz9DhjrMPUT_KTsAsNW81Ol8JQn6G56tLTCtOSFDPggOW7piw_DjboWGpZC20t7N8gOoC3uHk',
-    location: 'Shibuya, Tokyo'
-  },
-]
+type IProps = RouteComponentProps<{ id: string }>
 
-const removed: Array<string> = []
-let dbState = db
+const Voting: FC<IProps> = observer((props) => {
+    const roomId = props.match.params["id"];
+    const votingStore = useContext(VotingStore).room(roomId);
 
+    useEffect(() => {
+        votingStore.updateItems();
+    }, []);
 
-const Voting: FC = () => {
-    const [items, setItems] = useState(db)
-
-    const onSwipe = (direction: string, removeItem: string) => {
-      console.log('You swiped: ' + direction)
-
-      removed.push(removeItem)
-      if (direction === 'right') {
-          console.log('Like')
-      } else if (direction === 'left') {
-          console.log('dislike')
-      }
+    const onCardLeftScreen = (direction: string, item: string) => {
+        votingStore.vote(item, direction == "left" ? "like" : "dislike");
     }
 
-    const onCardLeftScreen = (name: string) => {
-        console.log(name + ' left the screen')
-        dbState = dbState.filter(item => item.name !== name)
-        setItems(dbState)
+    let content;
+    if (votingStore.items.size > 0) {
+        content = <div className="cardContainer">
+            {[...votingStore.items].map((item
+                ) =>
+                    <div className="swipe">
+                        <TinderCard onCardLeftScreen={(dir) => onCardLeftScreen(dir, item.id)}
+                                    preventSwipe={['up', 'down']}>
+                            <div style={{backgroundImage: 'url(' + item.imgURL + ')'}} className='card'>
+                                <h3>{item.name}</h3>
+                            </div>
+                        </TinderCard>
+                    </div>
+            )}
+        </div>;
+    } else {
+        const message = votingStore.voted ?
+            "Wait for other people to finished or click add button to add more items" :
+            "Click add button to add more items";
+        content = <div className="message">{message}</div>;
     }
 
     return (
         <Fragment>
             <Header hasBackButton buttons={[
+                <IconButton> <AddOutlinedIcon/> </IconButton>,
                 <IconButton> <EditOutlinedIcon/> </IconButton>
             ]}>
                 Group Name
             </Header>
-
-            <div className="cardContainer">
-                {items.map((item
-                    ) =>
-                    <div className="swipe">
-                        <TinderCard onSwipe={(dir) => onSwipe(dir, item.name)}
-                                    onCardLeftScreen={() => onCardLeftScreen(item.name)}
-                                    preventSwipe={['up', 'down']}>
-                            <div style={{ backgroundImage: 'url(' + item.imgURL + ')' }} className='card'>
-                                <h3>{item.name}</h3>
-                            </div>
-                        </TinderCard>
-                    </div>
-                )}
-
-            </div>
-
+            {content}
         </Fragment>
     )
-}
+})
 
 export default Voting;
