cd9046596d63899a1b76eb2d1ad1980b7d0b973d
eureyuri <eureynoguchi@gmail.com>
2020-11-17 09:41:35 -0500

    feat: skeleton for stats page

--- webapp/src/pages/Statistics/index.tsx
+++ webapp/src/pages/Statistics/index.tsx
@@ -2,6 +2,27 @@
  * @desc the results of a matching process.
  */
 
-import React from "react";
+import React, {FC, Fragment, useState} from "react";
+import Header from "../../components/Header";
+import IconButton from '@material-ui/core/IconButton';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import TinderCard from 'react-tinder-card'
 
-export { }
+
+
+const Statistics: FC = () => {
+    return (
+        <Fragment>
+            <Header hasBackButton buttons={[
+                <IconButton> <EditOutlinedIcon/> </IconButton>
+            ]}>
+                Group Name
+            </Header>
+
+            Stats
+
+        </Fragment>
+    )
+}
+
+export default Statistics;
