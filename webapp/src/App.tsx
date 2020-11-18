e0017c075bfd687810a333ad55c4d2e0b43043ab
eureyuri <eureynoguchi@gmail.com>
2020-11-17 21:05:40 -0500

    feat: landing page content

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ShibaLogo from "./components/ShibaLogo";
+import Authentication from "./pages/Authentication";
+import {Route} from "react-router";
+import {Button} from "@material-ui/core";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: 'white',
+    backgroundColor: '#FFBC6F',
+    '&:hover': {
+      backgroundColor: '#ffb35a',
+    },
+    width: '250px',
+    height: '60px',
+    borderRadius: '15px',
+    fontSize: '24px',
+    fontWeight: "bold",
+  },
+}))(Button);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div style={{ height: '200px' }}>
+          <ShibaLogo />
+        </div>
+
+        <h1>Find your soul</h1>
 
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Easily find a restaurant or hotel for your group.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <ColorButton variant="contained" color="primary" disableElevation>
+          Log In
+        </ColorButton>
+
+
       </header>
     </div>
   );
