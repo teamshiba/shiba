98327c990bcb59cbc2b3f1abb0d858b15133bba5
yifei <yifei529@gmail.com>
2020-11-18 18:32:01 -0500

    test: Header

---
+++ webapp/src/components/Header.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import Header from "./Header";
+
+test("renders", async () => {
+    render(<Header>Title</Header>);
+    const elem = screen.queryByText(/Title/i);
+    expect(elem).toBeInTheDocument();
+});
+
