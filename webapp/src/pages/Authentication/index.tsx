b2cf7ed3104e24ed676a7dcc5e34eba2c5d58a1b
yifei <yifei529@gmail.com>
2020-11-17 22:57:24 -0500

    feat: skip authentication page if the user is already logged in.

--- webapp/src/pages/Authentication/index.tsx
+++ webapp/src/pages/Authentication/index.tsx
@@ -2,14 +2,22 @@
  * @desc authentication page.
  */
 
-import React, { FC } from "react";
+import React, {FC, useEffect} from "react";
 import Box from '@material-ui/core/Box';
 
 import SignIn from "./SignIn";
 import ShibaLogo from "../../components/ShibaLogo";
 import Header from "../../components/Header";
+import {browserHistory} from "../../common/utils";
 
 const Authentication: FC = () => {
+    useEffect(() => {
+        const token = localStorage.getItem("auth_token");
+        if (token && token.length > 0) {
+            browserHistory.push("/room/active");
+        }
+    }, []);
+
     return (
         <>
             <Header>
