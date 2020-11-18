3b14249ec240851e35f9b4d5f52353114084849b
eureyuri <eureynoguchi@gmail.com>
2020-11-17 22:09:10 -0500

    fix: delete unnecessary lines

--- webapp/src/pages/Authentication/index.tsx
+++ webapp/src/pages/Authentication/index.tsx
@@ -4,11 +4,8 @@
 
 import React, { FC } from "react";
 import Box from '@material-ui/core/Box';
-// import Header from "../../components/Header";
 
 import SignIn from "./SignIn";
-import IconButton from "@material-ui/core/IconButton";
-import AddIcon from "@material-ui/icons/Add";
 import ShibaLogo from "../../components/ShibaLogo";
 import Header from "../../components/Header";
 
