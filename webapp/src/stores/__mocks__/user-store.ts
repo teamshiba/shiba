26d7ad7dcc0ef7d9de9c0f2907081f3280d0a260
yifei <yifei529@gmail.com>
2020-11-18 23:16:54 -0500

    test: UserProfile

--- webapp/src/stores/__mocks__/user-store.ts
+++ webapp/src/stores/__mocks__/user-store.ts
@@ -2,7 +2,12 @@ import {makeAutoObservable} from "mobx";
 import {User} from "../../domain/user";
 
 class UserStore {
-    user: User | null = null;
+    user = {
+        uid: "1234",
+        displayName: "Test User",
+        email: "test@test.com",
+    } as User
+
     initialized = false;
 
     constructor() {
