c6121acd94491319957d8e83a3ef802a459322c0
Chiqu Li <chiquli@163.com>
2020-12-07 01:23:34 -0500

    fix: modified pytest command

--- service/report.sh
+++ service/report.sh
@@ -4,4 +4,10 @@ pip install requirements.txt
 pylint --reports=y --disable=C0415,C0103 --msg-template="$FileDir$/{path}:{line}:{C}:({symbol}){msg}" service/models service/routes service/utils
  > reports/stylecheck-service
 flake8 ./service/ --format=html --htmldir=reports/bugfinder-service --statistics --ignore E501 --exclude='test_*.py'
-pytest --cov ./service/tests
+
+# pytest should be run from service
+cd service
+pytest --cov-report term-missing --cov={models,utils,routes}
+
+
+
