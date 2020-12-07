7b0973d2bc28a4e7ba2da2a1ec587dbfc8c4d7d1
Jianing <samosatar@gmail.com>
2020-12-07 10:51:13 +0800

    chore: auto report scripts.
    Signed-off-by: Jianing <samosatar@gmail.com>

--- service/report.sh
+++ service/report.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # This should be run from root directory (shiba)
 pip install requirements.txt
-pylint --reports=y --disable=import-outside-toplevel --disable=invalid-name --msg-template="$FileDir$/{path}:{line}:{C}:({symbol}){msg}" service/models service/routes service/utils
+pylint --reports=y --disable=C0415,C0103 --msg-template="$FileDir$/{path}:{line}:{C}:({symbol}){msg}" service/models service/routes service/utils
  > reports/stylecheck-service
-flake8 ./service/ --format=html --htmldir=reports/bugfinder-service --statistics --max-line-length=99
+flake8 ./service/ --format=html --htmldir=reports/bugfinder-service --statistics --ignore E501 --exclude='test_*.py'
+pytest --cov ./service/tests
